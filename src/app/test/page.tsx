197c79dcb745bdfaa9f215c630ab6241b1d10ec1
claustra01 <shinya430.g@gmail.com>
2023-05-21 00:42:25 +0900

    update: recieve and send

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,6 +4,14 @@ import { useEffect, useRef, useState } from 'react'
 const Test = () => {
   const socketRef = useRef<WebSocket>()
   const [isConnected, setIsConnected] = useState(false)
+  const [formMessage, setFormMessage] = useState('')
+  const [sentMessage, setSentMessage] = useState('')
+
+  const sendData = (event: any) => {
+    event.preventDefault()
+    setFormMessage(event.target[0].value)
+    socketRef.current?.send(event.target[0].value)
+  }
 
   useEffect(() => {
     socketRef.current = new WebSocket('ws://localhost:8001/socket')
@@ -17,6 +25,11 @@ const Test = () => {
       setIsConnected(false)
     }
 
+    // server 側から送られてきたデータを受け取る
+    socketRef.current.onmessage = function (event) {
+      setSentMessage(event.data)
+    }
+
     return () => {
       if (socketRef.current == null) {
         return
@@ -28,6 +41,12 @@ const Test = () => {
   return (
     <>
       <h1>WebSocket is connected : {`${isConnected}`}</h1>
+      <form onSubmit={sendData}>
+        <input type="text" name="socketData" />
+        <button type="submit">Server に送信</button>
+      </form>
+      <h3>form message: {formMessage}</h3>
+      <h3>sent message: {sentMessage}</h3>
     </>
   )
 }
