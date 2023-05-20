7f231bb92cf3d0ee286c2479887933228c3716fb
K-Kizuku <kotani.kizuku@gmail.com>
2023-05-21 05:47:24 +0900

    [add] ヘッダーの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 'use client';
 import Image from 'next/image';
 // import Streaming from '~/components/Stream';
-import Video from '~/components/Video';
 
 export default function Home() {
-  return <div>{/* <Video /> */}</div>;
+  return (
+    <div className='containner'>
+      <div className='stage'>
+        <div className='dice'>
+          <div className='item'>1</div>
+          <div className='item'>2</div>
+          <div className='item'>3</div>
+          <div className='item'>4</div>
+          <div className='item'>5</div>
+          <div className='item'>6</div>
+        </div>
+      </div>
+    </div>
+  );
 }
