bb80ec12059e7c14fa9fcac611d1a230baaca2ba
claustra01 <shinya430.g@gmail.com>
2023-05-21 02:12:44 +0900

    add: send json to server

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,25 +1,49 @@
 'use client';
 import { useEffect, useRef, useState } from 'react'
 
+// テスト用, 形式は不問, 一意でさえあればOK
+const streamId = "hogihogewiheqjpo"
+
+type MsgFromClient = {
+  stream_id: string
+  comment: string
+  reaction: boolean
+}
+
 const Test = () => {
   const socketRef = useRef<WebSocket>()
-  const [isConnected, setIsConnected] = useState(false)
+  const [isConnected, setIsConnected] = useState<boolean>(false)
   const [formMessage, setFormMessage] = useState('')
   const [sentMessage, setSentMessage] = useState('')
 
-  const sendData = (event: any) => {
-    event.preventDefault()
-    setFormMessage(event.target[0].value)
-    socketRef.current?.send(event.target[0].value)
+  const sendComment = (e: any) => {
+    e.preventDefault()
+    setFormMessage(e.target[0].value)
+    socketRef.current?.send(e.target[0].value)
+  }
+
+  const sendReaction = (e: any) => {
+
   }
 
   useEffect(() => {
     socketRef.current = new WebSocket('ws://localhost:8001/socket')
+
+    // 接続時の処理
     socketRef.current.onopen = function () {
+      // StreamIdを送る
+      const msg: MsgFromClient = {
+        stream_id: streamId,
+        comment: '',
+        reaction: false
+      }
+      socketRef.current?.send(JSON.stringify(msg))
+      console.log(JSON.stringify(msg))
       setIsConnected(true)
       console.log('Connected')
     }
 
+    // 切断時の処理
     socketRef.current.onclose = function () {
       console.log('closed')
       setIsConnected(false)
@@ -41,12 +65,16 @@ const Test = () => {
   return (
     <>
       <h1>WebSocket is connected : {`${isConnected}`}</h1>
-      <form onSubmit={sendData}>
+
+      <form onSubmit={sendComment}>
         <input type="text" name="socketData" />
-        <button type="submit">Server に送信</button>
+        <button type="submit">Comment</button>
       </form>
+      <button type="button" onClick={sendReaction}>Reaction</button>
+
       <h3>form message: {formMessage}</h3>
       <h3>sent message: {sentMessage}</h3>
+
     </>
   )
 }
