608cc65879516dcf3f92e9225b70281c999330fa
claustra01 <shinya430.g@gmail.com>
2023-05-21 00:26:32 +0900

    add: connect websocket

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,43 +1,35 @@
 'use client';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react'
 
 const Test = () => {
-  const [message, setMessage] = useState<string>();
-  const socketRef = useRef<WebSocket>();
+  const socketRef = useRef<WebSocket>()
+  const [isConnected, setIsConnected] = useState(false)
 
-  // #0.WebSocket関連の処理は副作用なので、useEffect内で実装
   useEffect(() => {
-    // #1.WebSocketオブジェクトを生成しサーバとの接続を開始
-    const websocket = new WebSocket('ws://localhost:5000');
-    socketRef.current = websocket;
+    socketRef.current = new WebSocket('ws://localhost:8001/socket')
+    socketRef.current.onopen = function () {
+      setIsConnected(true)
+      console.log('Connected')
+    }
 
-    // #2.メッセージ受信時のイベントハンドラを設定
-    const onMessage = (event: MessageEvent<string>) => {
-      setMessage(event.data);
-    };
-    websocket.addEventListener('message', onMessage);
+    socketRef.current.onclose = function () {
+      console.log('closed')
+      setIsConnected(false)
+    }
 
-    // #3.useEffectのクリーンアップの中で、WebSocketのクローズ処理を実行
     return () => {
-      websocket.close();
-      websocket.removeEventListener('message', onMessage);
-    };
-  }, []);
+      if (socketRef.current == null) {
+        return
+      }
+      socketRef.current.close()
+    }
+  }, [])
 
   return (
     <>
-      <div>最後に受信したメッセージ: {message}</div>
-      <button
-        type='button'
-        onClick={() => {
-          // #4.WebSocketでメッセージを送信する場合は、イベントハンドラ内でsendメソッドを実行
-          socketRef.current?.send('送信メッセージ');
-        }}
-      >
-        送信
-      </button>
+      <h1>WebSocket is connected : {`${isConnected}`}</h1>
     </>
-  );
-};
+  )
+}
 
-export default Test;
+export default Test
